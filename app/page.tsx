9e68b885906e42e147bb0b979bfe47f9d77eeebe
marleypm16 <yarlempm16@gmail.com>
2024-05-27 20:44:50 -0300

    feat : add ImageBanner in home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 import Header from "@/app/_components/header";
+import ImageBanner from "@/app/_components/ImageBanner";
 
 export default function Home() {
   return (
-      <Header/>
+      <>
+        <Header/>
+        <ImageBanner/>
+      </>
+
   );
 }
