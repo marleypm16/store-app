61b1d0323bcc6d64a2ffbeffa51ad26de50988ee
marleypm16 <yarlempm16@gmail.com>
2024-05-27 11:11:31 -0300

    feat : header component

---
+++ app/_components/header.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Button} from "@/app/_components/ui/button";
+import {MenuIcon, ShoppingCartIcon} from "lucide-react";
+import Image from "next/image";
+
+const Header = () => {
+    return (
+        <div className='p-5 border-b flex justify-between items-center'>
+            <Button variant='secondary'>
+                <MenuIcon/>
+            </Button>
+            <Image src='/images/logo.png' width={60} height={60} alt='logo'/>
+            <Button variant='secondary'>
+                <ShoppingCartIcon/>
+            </Button>
+        </div>
+    );
+};
+
+export default Header;
