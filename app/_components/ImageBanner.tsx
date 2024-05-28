18e197e113717a47f32e1f7e65c664efe2e4ba4a
marleypm16 <yarlempm16@gmail.com>
2024-05-28 09:17:27 -0300

    ImageBanner.tsx dinamic

--- app/_components/ImageBanner.tsx
+++ app/_components/ImageBanner.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Image from "next/image";
-
-const ImageBanner = () => {
+interface ImageBannerProps {
+    image: string;
+    alt : string
+}
+const ImageBanner = ({image,alt} :ImageBannerProps) => {
     return(
         <div className='p-5 text-center'>
-            <Image src='/images/banner-home-01.png'
-                   alt="Produtos com 55% de desconto"
+            <Image src={`/images/banner-home-${image}.png`}
+                   alt={alt}
                    width={0} height={0}
                    className='h-auto w-full object-cover'
                    sizes='100vw'/>
