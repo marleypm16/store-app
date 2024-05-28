60d8dc85c819d18937d4e00300b1e05fc2934cd6
marleypm16 <yarlempm16@gmail.com>
2024-05-27 21:06:14 -0300

    add category list in home

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import Header from "@/app/_components/header";
 import ImageBanner from "@/app/_components/ImageBanner";
+import CategoryList from "./_components/CategoryList";
 
 export default function Home() {
   return (
       <>
         <Header/>
         <ImageBanner/>
+          <CategoryList/>
+
       </>
 
   );
