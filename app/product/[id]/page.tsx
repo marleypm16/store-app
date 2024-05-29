33a05aa2b331e76c2d657473a6e55528b1f63fd8
marleypm16 <yarlempm16@gmail.com>
2024-05-29 10:14:07 -0300

    feat : add product page component

---
+++ app/product/[id]/page.tsx
@@ -0,0 +1,44 @@
+import { db } from '@/app/_lib/prisma';
+import React from 'react';
+import {notFound} from "next/navigation";
+import Header from "@/app/_components/header";
+import ProductDetails from "@/app/product/[id]/_components/productDetails";
+import ProductList from "@/app/_components/ProductList";
+import CartButton from "@/app/product/[id]/_components/cartButton";
+import ProductImages from "@/app/product/[id]/_components/ProductImages";
+interface ProductPageProps {
+    params: {
+        id: string;
+    }
+}
+const ProductPage = async ({params}:ProductPageProps) => {
+    const product = await db.product.findUnique({
+        where: {
+            id: params.id
+        }
+    })
+    const products = await db.product.findMany({take: 10});
+    if (!product) {
+        return  notFound();
+    }
+    return (
+        <>
+            <Header/>
+            <div  className='p-5 '>
+
+                    <ProductImages product={product}/>
+                <div className='mt-4'>
+                    <ProductDetails product={product}/>
+                    <CartButton/>
+                </div>
+                <div className='mt-4'>
+                    <h2 className='mb-4 '>Produtos que você também pode gostar</h2>
+                    <ProductList products={products}/>
+                </div>
+            </div>
+
+        </>
+    );
+};
+
+export default ProductPage;
