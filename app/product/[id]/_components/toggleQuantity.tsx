9fc69b46cfd4473ba6463fbc9e6965a92ab4e112
marleypm16 <yarlempm16@gmail.com>
2024-06-03 09:14:51 -0300

    feat : increment,decrement functions context

--- app/product/[id]/_components/toggleQuantity.tsx
+++ app/product/[id]/_components/toggleQuantity.tsx
@@ -2,17 +2,11 @@
 import React from 'react';
 import {Button} from "@/app/_components/ui/button";
 import {ArrowLeftIcon, ArrowRightIcon} from "lucide-react";
+import {useCart} from "@/app/_context/cartContext";
 
 const ToggleQuantity = () => {
-    const [quantity, setQuantity] = React.useState<number>(1);
-    const handleIncrement = () => {
-        setQuantity(quantity + 1);
-    }
-    const handleDecrement = () => {
-        if (quantity > 1) {
-            setQuantity(quantity - 1);
-        }
-    }
+    const {handleIncrement, handleDecrement, quantity} = useCart();
+
     return (
         <div className='flex items-center gap-5'>
             <Button onClick={handleDecrement} size='icon'><ArrowLeftIcon/></Button>
