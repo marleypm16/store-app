66ca58e69d96792d519ac77c70298973c46d1941
marleypm16 <yarlempm16@gmail.com>
2024-06-03 09:18:09 -0300

    .

--- app/_components/ImageBanner.tsx
+++ app/_components/ImageBanner.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Image from "next/image";
+import Link from "next/link";
 interface ImageBannerProps {
     image: string;
     alt : string
@@ -7,11 +8,14 @@ interface ImageBannerProps {
 const ImageBanner = ({image,alt} :ImageBannerProps) => {
     return(
         <div className='p-5 text-center'>
-            <Image src={`/images/banner-home-${image}.png`}
-                   alt={alt}
-                   width={0} height={0}
-                   className='h-auto w-full object-cover'
-                   sizes='100vw'/>
+                <Image
+                    src={`/images/banner-home-${image}.png`}
+                    alt={alt}
+                    width={0} height={0}
+                    className='h-auto w-full object-cover'
+                    sizes='100vw'
+                />
+
         </div>
     );
 };
