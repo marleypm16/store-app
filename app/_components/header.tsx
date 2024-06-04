e7e1e93727699bb227f10e0b26565b15f58ef982
marleypm16 <yarlempm16@gmail.com>
2024-06-04 16:04:00 -0300

    add user data

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,13 +1,20 @@
+"use client";
 import React from 'react';
 import {Button} from "@/app/_components/ui/button";
-import {BookTextIcon, HomeIcon, MenuIcon} from "lucide-react";
+import {BookTextIcon, HomeIcon, LogInIcon, LogOutIcon, MenuIcon} from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import {Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger} from "@/app/_components/ui/sheet";
 import Cart from "@/app/_components/cart";
 import CartIconButton from "@/app/_components/CartIconButton";
+import {signIn,signOut,useSession} from "next-auth/react";
+import {Separator} from "@/app/_components/ui/separator";
+import {Avatar, AvatarFallback, AvatarImage} from "@/app/_components/ui/avatar";
 
 const Header = () => {
+    const {data} = useSession();
+    const handleSignIn= () => signIn("github");
+    const handleSignOut = () => signOut();
     return (
         <div className='p-5 border-b flex justify-between items-center'>
             <Sheet>
@@ -18,6 +25,41 @@ const Header = () => {
                 <SheetContent side='left'>
                     <SheetHeader>
                         <SheetTitle>Menu</SheetTitle>
+                        {data?.user ? (
+                            <>
+                                <div className="flex justify-between pt-6">
+                                    <div className="flex items-center gap-3">
+
+
+                                        <div className='flex items-center gap-4 '>
+                                            <Avatar>
+                                                <AvatarImage src={data?.user?.image as string | undefined} />
+                                                <AvatarFallback>{data?.user?.name}</AvatarFallback>
+                                            </Avatar>
+                                            <div>
+                                                <h3 className="font-semibold">{data?.user?.name}</h3>
+                                                <span className="block text-xs text-muted-foreground">
+                                                    {data?.user?.email}
+                                            </span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </>
+                        ) : (
+                            <>
+                            <div className="flex items-center justify-between pt-10">
+                                    <h2 className="font-semibold">Olá. Faça seu login!</h2>
+                                    <Button size="icon" onClick={handleSignIn}>
+                                        <LogInIcon />
+                                    </Button>
+                                </div>
+                            </>
+                        )}
+
+                        <div className="py-6">
+                            <Separator />
+                        </div>
                         <SheetDescription>
                             <div className="flex flex-col gap-3 px-5">
                                 <Button variant="outline" className="justify-start" asChild>
@@ -26,13 +68,25 @@ const Header = () => {
                                         Início
                                     </Link>
                                 </Button>
-                                <Button variant="outline" className="justify-start" asChild>
-                                    <Link href={`/orders`}>
-                                        <BookTextIcon size={18} className="mr-2"/>
-                                        Pedidos
-                                    </Link>
-                                </Button>
+                                {data?.user && (
+                                    <Button variant="outline" className="justify-start" asChild>
+                                        <Link href={`/orders`}>
+                                            <BookTextIcon size={18} className="mr-2"/>
+                                            Pedidos
+                                        </Link>
+                                    </Button>
+                                )}
                             </div>
+                            {data?.user && (
+                                <Button
+                                    variant="outline"
+                                    className="w-full justify-start space-x-3  text-sm font-norma mt-4"
+                                    onClick={handleSignOut}
+                                >
+                                    <LogOutIcon size={16} />
+                                    <span className="block">Sair da conta</span>
+                                </Button>
+                            )}
                         </SheetDescription>
                     </SheetHeader>
                 </SheetContent>
