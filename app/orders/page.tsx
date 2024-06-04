2f5fd79a90891a6f31f76a026797b31f288c83d4
marleypm16 <yarlempm16@gmail.com>
2024-06-04 09:49:15 -0300

    feat :add OrdersItem component

--- app/orders/page.tsx
+++ app/orders/page.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import {db} from "@/app/_lib/prisma";
 import Header from "@/app/_components/header";
-import {Format} from "@/app/_lib/format";
-import {Card, CardContent} from "@/app/_components/ui/card";
-import Link from "next/link";
+
+import OrdersItem from "@/app/orders/_components/OrdersItem";
 
 const OrdersPage = async () => {
     const orders = await db.order.findMany(
@@ -13,23 +12,13 @@ const OrdersPage = async () => {
             }
         }
     );
+
     return (
         <>
             <Header/>
             <h2 className='text-center p-5'>Pedidos</h2>
             {orders.map(order => (
-                <Link key={order.id} className='mx-3' href={`/orders/${order.id}`}>
-                    <Card className='p-5'>
-                        <CardContent>
-                            <p># {order.id}</p>
-                            <p>Data: {Format.formatDate(order.createdAt)}</p>
-                            <p>Quantidade: {order.totalQuantity}</p>
-                            <p>Valor total: {Format.formatPrice(Number(order.totalPrice))}</p>
-                            <p>Status: {order.status}</p>
-                        </CardContent>
-                    </Card>
-                </Link>
-
+                <OrdersItem key={order.id} order={order}></OrdersItem>
             ))}
         </>
     );
