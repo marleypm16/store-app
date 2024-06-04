d21560466de95f712f10c3cce31088e33490728f
marleypm16 <yarlempm16@gmail.com>
2024-06-04 16:35:49 -0300

    add format functions

--- app/_lib/format.ts
+++ app/_lib/format.ts
@@ -1,27 +1,16 @@
 import {CartProduct} from "@/app/_context/cartContext";
 import {format} from "date-fns";
-export class Format {
 
-    static calculateTotalPrice(cart:CartProduct[]) : number{
-        return cart.reduce((acc, item) => acc + item.totalPrice, 0)
-    }
-    static calculateTotalQuantity(cart:CartProduct[]) : number{
-        return cart.reduce((acc, item) => acc + item.quantity, 0)
-    }
-    static formatDate(date: Date): string {
-            return format(new Date(date), 'dd/MM/yyyy')
-    }
-    static formatPrice(basePrice: number): string {
+export const formatPrice = (basePrice: number): string => {
         return Intl.NumberFormat('pt-br', {
             style: 'currency',
             currency: 'BRL',
             minimumFractionDigits: 2
         }).format(basePrice)
     }
-    static calculateDiscount(basePrice: number, discountPercentage: number): number {
-        return basePrice - (basePrice * discountPercentage / 100)
-    }
 
+
+
+export const formatDate = (date: Date): string => {
+    return format(new Date(date), 'dd/MM/yyyy')
 }
-
-
