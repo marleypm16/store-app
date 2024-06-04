532093e79111c52c1559aa09f74616e6e183e180
marleypm16 <yarlempm16@gmail.com>
2024-06-04 16:37:53 -0300

    .

--- app/orders/page.tsx
+++ app/orders/page.tsx
@@ -3,10 +3,20 @@ import {db} from "@/app/_lib/prisma";
 import Header from "@/app/_components/header";
 
 import OrdersItem from "@/app/orders/_components/OrdersItem";
+import {getServerSession} from "next-auth";
+import {authOptions} from "@/app/_lib/auth";
+import { redirect } from "next/navigation";
 
 const OrdersPage = async () => {
+    const session = await getServerSession(authOptions)
+    if(!session){
+        return redirect('/')
+    }
     const orders = await db.order.findMany(
         {
+            where:{
+                userId:session.user.id
+            },
             include:{
                 orderProduct: {
                     include:{
@@ -24,7 +34,6 @@ const OrdersPage = async () => {
 
             <div className="px-5 py-6">
                 <h2 className="pb-6 text-lg font-semibold">Meus Pedidos</h2>
-
                 <div className="space-y-4  ">
                     {orders.map((order) => (
                         <OrdersItem key={order.id} order={order} />
